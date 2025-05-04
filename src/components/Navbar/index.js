af4c12c78d2c1b31c7688e5a2e41393bd8ff610d
Mitch Hartigan <42709932+mitchHartigan@users.noreply.github.com>
2025-05-04 14:26:48 -0700

    content: doc linkkk

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,6 +1,7 @@
 import styles from "./navbar.module.css";
 
 export function Navbar() {
+  // https://docs.google.com/document/d/1V1aJwcV5yEgC7vC2CjhXSMoNPV1ZQRSS/edit?usp=sharing&ouid=108301802730684787537&rtpof=true&sd=true
   return (
     <div className={styles.container}>
       <Image src="Logo.png" alt="Logo" />
