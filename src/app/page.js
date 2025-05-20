d0243d183831135af0cce450822cb67559f74071
Mitch Hartigan Win11 <42709932+mitchHartigan@users.noreply.github.com>
2025-05-20 15:38:49 -0700

    feat: call to action section

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,15 @@
 "use client";
 import Image from "next/image";
 import { Hero } from "../components/Hero";
+import { Navbar } from "@/components/Navbar";
+import { CallToAction } from "@/components/CallToAction";
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <Hero />
+      <CallToAction />
     </div>
   );
 }
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,14 +1,17 @@
+import Image from "next/image";
 import styles from "./navbar.module.css";
 
 export function Navbar() {
   // https://docs.google.com/document/d/1V1aJwcV5yEgC7vC2CjhXSMoNPV1ZQRSS/edit?usp=sharing&ouid=108301802730684787537&rtpof=true&sd=true
   return (
     <div className={styles.container}>
-      <Image src="Logo.png" alt="Logo" />
-      <div className={styles.linksContainer}>
-        <p>one link</p>
-        <p>two link</p>
-        <p>three link</p>
+      <div className={styles.content}>
+        <Image src="/Logo.png" alt="Logo" width={114} height={41} />
+        <div className={styles.links}>
+          <p>one link</p>
+          <p>two link</p>
+          <p>three link</p>
+        </div>
       </div>
     </div>
   );
